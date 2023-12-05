a9f84094edf8ed0ff674adf0b83dc6a9001ec379
Edward Cronin <77951297+hashkeeper@users.noreply.github.com>
2023-12-05 10:33:36 -0500

    fixes

--- randomgen.sh
+++ randomgen.sh
@@ -21,106 +21,121 @@ do
 	esac 
 done
 
-if [[ "$lowerc" = true ]] && [[ "$upperc" = false ]] && [[ "$num" = false ]] && [[ "$special" = false ]]; then
+if [ "$lowerc" = true ] && [ "$upperc" = false ] && [ "$num" = false ] && [ "$special" = false ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
-			echo -n "${lchars:RANDOM%${#lchars}:1}"
+			echo -n "${lchars:RANDOM%${#lchars}:1}" 
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = false ]] && [[ "$upperc" = true ]] && [[ "$num" = false ]] && [[ "$special" = false ]]; then
+elif [ "$lowerc" = false ] && [ "$upperc" = true ] && [ "$num" = false ] && [ "$special" = false ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
   			echo -n "${uchars:RANDOM%${#uchars}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = false ]] && [[ "$upperc" = false ]] && [[ "$num" = true ]] && [[ "$special" = false ]]; then
+elif [ "$lowerc" = false ] && [ "$upperc" = false ] && [ "$num" = true ] && [ "$special" = false ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
   			echo -n "${nchars:RANDOM%${#nchars}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = false ]] && [[ "$upperc" = false ]] && [[ "$num" = false ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = false ] && [ "$upperc" = false ] && [ "$num" = false ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
   			echo -n "${schars:RANDOM%${#schars}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = true ]] && [[ "$upperc" = true ]] && [[ "$num" = false ]] && [[ "$special" = false ]]; then
+elif [ "$lowerc" = true ] && [ "$upperc" = true ] && [ "$num" = false ] && [ "$special" = false ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$lchars$uchars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = true ]] && [[ "$upperc" = false ]] && [[ "$num" = true ]] && [[ "$special" = false ]]; then
+elif [ "$lowerc" = true ] && [ "$upperc" = false ] && [ "$num" = true ] && [ "$special" = false ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do	
 			master=$lchars$nchars
 			echo -n "${master:RANDOM%${#master}:1}"
-		done
+	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = true ]] && [[ "$upperc" = false ]] && [[ "$num" = false ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = true ] && [ "$upperc" = false ] && [ "$num" = false ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$lchars$schars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = false ]] && [[ "$upperc" = true ]] && [[ "$num" = true ]] && [[ "$special" = false ]]; then
+elif [ "$lowerc" = false ] && [ "$upperc" = true ] && [ "$num" = true ] && [ "$special" = false ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$uchars$nchars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = false ]] && [[ "$upperc" = true ]] && [[ "$num" = false ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = false ] && [ "$upperc" = true ] && [ "$num" = false ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$uchars$schars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = false ]] && [[ "$upperc" = false ]] && [[ "$num" = true ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = false ] && [ "$upperc" = false ] && [ "$num" = true ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$nchars$schars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = true ]] && [[ "$upperc" = true ]] && [[ "$num" = true ]] && [[ "$special" = false ]]; then
+elif [ "$lowerc" = true ] && [ "$upperc" = true ] && [ "$num" = true ] && [ "$special" = false ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$lchars$uchars$nchars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = true ]] && [[ "$upperc" = true ]] && [[ "$num" = false ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = true ] && [ "$upperc" = true ] && [ "$num" = false ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$lchars$uchars$schars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = true ]] && [[ "$upperc" = false ]] && [[ "$num" = true ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = true ] && [ "$upperc" = false ] && [ "$num" = true ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$lchars$nchars$schars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = false ]] && [[ "$upperc" = true ]] && [[ "$num" = true ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = false ] && [ "$upperc" = true ] && [ "$num" = true ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$uchars$nchars$schars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
-elif [[ "$lowerc" = true ]] && [[ "$upperc" = true ]] && [[ "$num" = true ]] && [[ "$special" = true ]]; then
+elif [ "$lowerc" = true ] && [ "$upperc" = true ] && [ "$num" = true ] && [ "$special" = true ]; then
 	for (( i = 0; i <= $numb; i++ ))
 		do
 			master=$lchars$uchars$nchars$schars
 			echo -n "${master:RANDOM%${#master}:1}"
 	done
+	echo \n
 	exit 0
 else 
 	echo 'options: {l}ower-case letters, {u}pper-case letters, {n}umbers, {s}pecial characters'
--- rangen.sh
+++ rangen.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/bash
-numb="$2"
+numb=$(($2-1))
 
 lchars=$'abcdefghijklmnopqrstuvwxyz'
 uchars=$'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbs=$'1234567890'
 schars=$'0=+-\'`"[]{}()<>;:,./~!@#$%^&*_|?\\'
 
+masarr=$''
 finarr=$''
 
-while getopts ":l:u:n:s:" flag
+while getopts "luns:" flag
 do
 	case "${flag}" in
-		l) finarr+=${lchars};;
-		u) finarr+=${uchars};;
-		n) finarr+=${numbs};;
-		s) finarr+=${schars};;
+		l) masarr+=${lchars};;
+		u) masarr+=${uchars};;
+		n) masarr+=${numbs};;
+		s) masarr+=${schars};;
 	esac 
+done
+
+for (( i = 0; i <= $numb; i++ ))
+	do
+		finarr="$finarr${masarr:RANDOM%${#masarr}:1}"
 done
 
 echo $finarr
